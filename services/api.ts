04a515c897dca7ffc4bc2020654680f4f7cecd38
rvanji <72876394+rvanji@users.noreply.github.com>
2026-08-12 17:04:27 +0530

    api url update

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://cube-tracker-api-backend.onrender.com",
+  baseURL: "https://cube-tracker-api-backend.onrender.com/api",
 });
 
 export default api;
